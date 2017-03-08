b2e53916ff5708c07a218c046cdc8db3a30cbfdc
Viktor Borisov <vik.borisov@outlook.com>
2017-03-08 15:34:15 +0700

    modify enumerable performance test

---
+++ BarsGroup.CodeGuard.PerfomanceTests/PerfTestHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+using Xunit;
+
+namespace BarsGroup.CodeGuard.PerfomanceTests
+{
+    internal static class PerfTestHelper
+    {
+        public static void RunTest(double maxCoef, Action guardValidator, Action nativeValidator )
+        {
+            var watch = new Stopwatch();
+            watch.Start();
+            for (var i = 0; i < 10000000; i++)
+            {
+                guardValidator();
+            }
+            watch.Stop();
+
+            var guardTime = watch.ElapsedMilliseconds;
+
+            watch.Reset();
+            watch.Start();
+            for (var i = 0; i < 10000000; i++)
+            {
+                nativeValidator();
+            }
+
+            watch.Stop();
+            var natimeTime = watch.ElapsedMilliseconds;
+
+            Assert.True(Math.Round((float)guardTime / natimeTime, 2) <= maxCoef, $"Guard time - {guardTime}, native time - {natimeTime}");
+        }
+
+    }
+}
---
+++ BarsGroup.CodeGuard.PerfomanceTests/Validators/EnumerableTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BarsGroup.CodeGuard.PerfomanceTests.Attibutes;
+using BarsGroup.CodeGuard.Validators;
+using Xunit;
+
+namespace BarsGroup.CodeGuard.PerfomanceTests.Validators
+{
+    public class EnumerableTests
+    {
+        readonly IEnumerable<string> _enumerable = Enumerable.Repeat(string.Empty, 10);
+
+        //[Theory]
+        //[Repeat(10)]
+        [Fact]
+        public void IsNotEmpty()
+        {
+            var guardValidator = new Action(() => Guard.That(_enumerable).IsNotEmpty());
+            var nativeValidator = new Action(() =>
+            {
+                // ReSharper disable once UseMethodAny.2
+                if (_enumerable.Count() == 0)
+                { 
+                    throw new Exception();
+                }
+            });
+
+            PerfTestHelper.RunTest(3.2, guardValidator, nativeValidator);
+        }
+    }
+}
